c0e0f5d9e39034dcb1af686477d1d73560f05900
Andy Babcock <andybabcock@jtof.dev>
2025-10-14 15:03:53 -0700

    feat: add initial pin configuration and notes

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## notes to self
 
 - potentiometer will output values between 0 and 1023
+
+### DRV8871 motor driver
+
+- forward: IN1 = PWM signal, IN2 = LOW
+- reverse: IN1 = LOW, IN2 = PWM signal
+- brake (Stop): IN1 = LOW, IN2 = LOW
--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,68 @@
 const int POTENTIOMETER_PIN = A0;
 
+const int LEFT_MOTOR_IN1 = 3;
+const int LEFT_MOTOR_IN2 = 5;
+const int RIGHT_MOTOR_IN1 = 6;
+const int RIGHT_MOTOR_IN2 = 9;
+
+const int DEAD_ZONE_LOW = 492;
+const int DEAD_ZONE_HIGH = 532;
+
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Potentiometer reader initialized. Turn the knob to see values change.");
+  Serial.println("Robot Cart Controller Initialized");
+
+  pinMode(LEFT_MOTOR_IN1, OUTPUT);
+  pinMode(LEFT_MOTOR_IN2, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN1, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN2, OUTPUT);
+
+  stopMotors();
 }
 
-void loop() {
-  int sensorValue = 0;
 
-  sensorValue = analogRead(POTENTIOMETER_PIN);
-  Serial.print("Analog Value: ");
-  Serial.println(sensorValue);
+void loop() {
+  int potValue = analogRead(POTENTIOMETER_PIN);
+
+  Serial.print("Potentiometer Value: ");
+  Serial.println(potValue);
 
   delay(100);
 }
+
+
+void setMotors(int speed) {
+  if (speed > 0) {
+    analogWrite(LEFT_MOTOR_IN1, speed);
+    digitalWrite(LEFT_MOTOR_IN2, LOW);
+    analogWrite(RIGHT_MOTOR_IN1, speed);
+    digitalWrite(RIGHT_MOTOR_IN2, LOW);
+    Serial.print("Forward - Speed: ");
+    Serial.println(speed);
+    
+  } else if (speed < 0) {
+    digitalWrite(LEFT_MOTOR_IN1, LOW);
+    analogWrite(LEFT_MOTOR_IN2, abs(speed));
+    digitalWrite(RIGHT_MOTOR_IN1, LOW);
+    analogWrite(RIGHT_MOTOR_IN2, abs(speed));
+    Serial.print("Reverse - Speed: ");
+    Serial.println(abs(speed));
+    
+  } else {
+    stopMotors();
+  }
+}
+
+
+/**
+ * @brief Stops all motors by setting both control pins LOW (brake).
+ */
+void stopMotors() {
+  digitalWrite(LEFT_MOTOR_IN1, LOW);
+  digitalWrite(LEFT_MOTOR_IN2, LOW);
+  digitalWrite(RIGHT_MOTOR_IN1, LOW);
+  digitalWrite(RIGHT_MOTOR_IN2, LOW);
+  Serial.println("Motors Stopped");
+}
+
