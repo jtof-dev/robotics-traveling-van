2cf67656bfc6c8f51ad737f08e8840eb8eadcc85
Andy Babcock <andybabcock@jtof.dev>
2025-11-28 15:15:10 -0700

    feat: map outputted motor values to a usable range

--- main/main.ino
+++ main/main.ino
@@ -4,32 +4,35 @@
 // setting up the physical pins
 // for the motor pins, they have to be PWM pins
 const int potPin = A0;
-const int encoderPinA = A1;
-const int encoderPinB = A2;
-const int leftMotorIn1 = 3;
-const int leftMotorIn2 = 5;
+const int encoderPinA = 2;
+const int encoderPinB = 3;
+const int leftMotorIn1 = 5;
+const int leftMotorIn2 = 6;
 // const int rightMotorIn1 = 6;
 // const int rightMotorIn2 = 9;
 
-double output, scaleFactor;
-double setPoint = 0;
+double output;
+double mappedOutput;
+double scaleFactor;
+// use this variable to set what angle the pot reads while the pendulum is perfectly balanced
+const double setPoint = -10;
 
 // define constants for the encoder
 const int countsPerRevolution = 520 * 4.0;
-const int maxDistanceMM = 457;
+int maxDistanceMM = 12.f * 25.4f;
 volatile long encoderCount = 0;
 long robotDistanceMM = 0;
 // int isRobotWithinMaxDistance = 1;
 
 // define constants for correlating distance and encoder values
 const float wheelDiameterMM = 65.0;
-const float wheelCircumferenceMM = PI * wheelCircumferenceMM;
+const float wheelCircumferenceMM = PI * wheelDiameterMM;
 const float mmPerCount = wheelCircumferenceMM / countsPerRevolution;
 
 // very preliminary values for Kp, Ki, and Kd. will require lots of tinkering
-double Kp = 4.9;
-double Ki = 0.1;
-double Kd = 0.4;
+const double Kp = 4.9;
+const double Ki = 0.1;
+const double Kd = 0.4;
 
 double angle;
 double motorCommand;
@@ -42,20 +45,20 @@ PID pid(&angle, &output, &setPoint, Kp, Ki, Kd, REVERSE);
 void setMotors() {
   // the output from the PID controller is between 0 and 255, so the direction is chosen based on whether angle is pos or neg
   if (output > 0) {
-    analogWrite(leftMotorIn1, abs(output));
+    analogWrite(leftMotorIn1, mappedOutput);
     digitalWrite(leftMotorIn2, LOW);
 
-    // analogWrite(rightMotorIn1, abs(output));
+    // analogWrite(rightMotorIn1, mappedOutput);
     // digitalWrite(rightMotorIn2, LOW);
 
     Serial.print("forward - PWM value: ");
     Serial.println(output);
   } else if (output < 0) {
     digitalWrite(leftMotorIn1, LOW);
-    analogWrite(leftMotorIn2, abs(output));
+    analogWrite(leftMotorIn2, mappedOutput);
 
     // digitalWrite(rightMotorIn1, LOW);
-    // analogWrite(rightMotorIn2, abs(output));
+    // analogWrite(rightMotorIn2, mappedOutput);
 
     Serial.print("reverse - PWM value: ");
     Serial.println(output);
@@ -143,25 +146,43 @@ void setup() {
 void loop() {
   robotDistanceMM = getDistanceTraveledMM();
 
-  if (fabs(robotDistanceMM < maxDistanceMM)) {
+  if (fabs(robotDistanceMM) < maxDistanceMM) {
     angle = map(analogRead(potPin), 0, 1023, -900, 900) / 10.0;
     Serial.print("angle: ");
     Serial.println(angle);
+
+    if (abs(angle) > 45) {
+      stopMotors();
+      maxDistanceMM = 0;
+    }
 
     // throw in the angle and get out a value between 0 and 255 (by default). the arduino by default only sends 8-bit PWM values (0 - 255)
     pid.Compute();
 
     Serial.print("PID controller output: ");
     Serial.println(output);
+    Serial.print("\n");
+
+    Serial.print("distance travelled: ");
+    Serial.println(robotDistanceMM);
   
+    // map the output range onto [70, 255] 
+    if (output > 0) {
+      mappedOutput = map(output, 0, 255, 70, 255);
+    } 
+    else if (output < 0) {
+      mappedOutput = map(output, -255, 0, -255, -70);
+    } 
+    // snap low values to 0 to avoid extra oscillations
+    else if (abs(output) < 2) {
+      mappedOutput = 0;
+    };
+
     // now, check if the robot is within a certain distance of the max distance and add some extra motor power if so (for future)
-    
-    // possibly, multiply or divide the output by a constant since four motor drivers are reading from the same pins
-    // output = output / 4;
 
     setMotors();
 
-    delay(500);
+    // delay(500);
   } else {
     stopMotors();
     delay(2000);
